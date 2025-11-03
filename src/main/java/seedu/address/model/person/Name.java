433096148413edc40e68348c4effcb5a3aa70f0b
gordon <gordontaylol@gmail.com>
2025-11-03 20:50:01 +0800

    Update person name field to allow dashes

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,14 +10,14 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters, commas, full-stops, apostrophes "
+            "Names should only contain alphanumeric characters, commas, full-stops, apostrophes, dashes "
             + "and spaces, and it should not be blank";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}](?:[\\p{Alnum} .,']*[\\p{Alnum}.])?";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}](?:[\\p{Alnum} .,'-]*[\\p{Alnum}.])?";
 
     public final String fullName;
 
